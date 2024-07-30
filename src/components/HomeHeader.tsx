43f959b5024d54a67f54a84bcf1ae01fd2899be9
Raphael Muniz <raphael.muniz@ifrn.edu.br>
2024-07-30 15:26:00 -0300

    feat: Add UserPhoto and HomeHeader components
    This commit adds the UserPhoto and HomeHeader components to the project. The UserPhoto component is responsible for rendering a user's photo with customizable styling, while the HomeHeader component displays a header section with the user's name and a logout button.

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,26 @@
+import { Heading, HStack, Icon, Text, VStack } from "@gluestack-ui/themed";
+import { LogOut } from "lucide-react-native";
+import { UserPhoto } from "./UserPhoto";
+
+export function HomeHeader() {
+  return (
+    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
+      <UserPhoto
+        source={{ uri: "https://github.com/raphaeldecm.png" }}
+        w="$16"
+        h="$16"
+        alt="Imagem do usuário"
+      />
+
+      <VStack flex={1}>
+        <Text color="$gray100" fontSize="$sm">
+          Olá,
+        </Text>
+        <Heading color="$gray100" fontSize="$md">
+          Raphael Muniz
+        </Heading>
+      </VStack>
+      <Icon as={LogOut} color="$gray200" size="xl" />
+    </HStack>
+  );
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,68 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import { ExerciseCard } from "@components/ExerciseCard";
+import { Group } from "@components/Group";
+import { HomeHeader } from "@components/HomeHeader";
+import { Text, Heading, HStack, VStack } from "@gluestack-ui/themed";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export function Home() {
+  const [exercises, setExercises] = useState([
+    "Puxada frontal",
+    "Remada curvada",
+    "Puxada alta",
+    "Remada baixa",
+    "Pulldown",
+    "Puxada supinada",
+    "Puxada neutra",
+  ]);
+  const [groups, setGroups] = useState([
+    "costas",
+    "ombro",
+    "peito",
+    "perna",
+    "bíceps",
+    "tríceps",
+  ]);
+  const [groupSelected, setGroupSelected] = useState("costas");
+
   return (
-    <Center flex={1}>
-      <Text color="white">Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected.toLowerCase() === item.toLowerCase()}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 32 }}
+        style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+      />
+
+      <VStack px="$8" flex={1}>
+        <HStack justifyContent="space-between" mb="$5" alignItems="center">
+          <Heading color="$gray200" fontSize="$md">
+            Exercícios
+          </Heading>
+          <Text color="$gray200" fontSize="$sm" fontFamily="$body">
+            {exercises.length} exercícios
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={() => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 32 }}
+        />
+      </VStack>
+    </VStack>
   );
 }

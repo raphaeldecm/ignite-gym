21de2370d80dfcd25ad6aee9108cc9bfc7be0321
Raphael Muniz <raphael.muniz@ifrn.edu.br>
2024-07-31 14:41:17 -0300

    feat: Add expo-image-picker plugin for accessing and selecting photos
    This commit adds the expo-image-picker plugin to the project, allowing users to access and select photos from their device's gallery. The plugin is configured to request the necessary permissions to access the user's photos.

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -5,7 +5,37 @@ import { UserPhoto } from "@components/UserPhoto";
 import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
 import { ScrollView, TouchableOpacity } from "react-native";
 
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+
+import { useState } from "react";
+
 export function Profile() {
+  const [userPhoto, setUserPhoto] = useState(
+    "https://github.com/raphaeldecm.png"
+  );
+
+  async function handleSelectImage() {
+    const photoSelected = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    });
+    if (photoSelected.canceled) {
+      return;
+    }
+    const photoUri = photoSelected.assets[0].uri;
+
+    if (photoUri) {
+      const photoInfo = (await FileSystem.getInfoAsync(photoUri)) as {
+        size: number;
+      };
+
+      setUserPhoto(photoSelected.assets[0].uri);
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
@@ -13,11 +43,11 @@ export function Profile() {
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt="$6" px="$10">
           <UserPhoto
-            source={{ uri: "https://github.com/raphaeldecm.png" }}
+            source={{ uri: userPhoto }}
             size="xl"
             alt="Imagem do usuário"
           />
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleSelectImage}>
             <Text
               color="$green500"
               fontFamily="$heading"
